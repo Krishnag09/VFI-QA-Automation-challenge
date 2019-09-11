4b6448d1ec9d28f131d62e927caa5eea2a5fdd1d
Krishna Gaurav <krishna@Krishnas-MBP.lan>
2019-09-11 12:43:45 -0400

    changes to the test

--- cypress/integration/end-to-end-2.spec.js
+++ cypress/integration/end-to-end-2.spec.js
@@ -5,11 +5,11 @@ describe('End to end Test 2', () => {
 	beforeEach(function(){
     cy.visit('https://www.binance.com/en/trade/ETH_BTC')
   })
-		it('Navigate to page', function () {
+		it('Navigate to ETH/BTC page', function () {
 			const price = cy.get('#FormRow-BUY-price').invoke('val')// 
-			cy.get ('#FormRow-BUY-quantity').type("10")
+			cy.get ('#FormRow-BUY-quantity').type("10") //putting a buy quantity
 			const testCreditBalance = price * 10
-			cy.get('#FormRow-BUY-total').invoke('val').should('eq', 0.17778000)
+			cy.get('#FormRow-BUY-total').invoke('val').should('eq', testCreditBalance) //asserting the calculated total price
 
 	})
 
